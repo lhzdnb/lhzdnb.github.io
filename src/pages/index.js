aa5ef61bc447b424ce5dd5f47f3f7f05a20dcaed
lh20020727 <lh20020727@sjtu.edu.cn>
2024-04-13 02:12:23 -0700

    删除文件

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/fundamental/intro"
           >
             现在就开始学习！ ⏱️
           </Link>
